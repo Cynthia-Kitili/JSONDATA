93fc3835c2eb28c406793688a9d3d7bdf76553e1
Kitili <KEN20971@kcbgroup.com>
2023-01-30 10:32:33 +0300

    add initial commit

---
+++ src/main/java/com/main/JSONDATA/JsondataApplication.java
@@ -0,0 +1,13 @@
+package com.main.JSONDATA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsondataApplication.class, args);
+	}
+
+}
